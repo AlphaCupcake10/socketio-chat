3430106f28ac9672545af5f787d494ae2259d11f
AlphaCupcake10 <alphacupcake@outlook.com>
2024-03-19 19:32:58 +0530

    Client

--- client/src/pages/ChatPage.tsx
+++ client/src/pages/ChatPage.tsx
@@ -2,31 +2,54 @@ import Button from "../components/common/Button"
 import Input from "../components/common/Input"
 import { useState } from "react";
 
+type MessageType = {
+    message: string,
+    sender: 0 | 1 | 2 // 0 for system, 1 for user, 2 for others
+    senderName?: string
+}
+
 function ChatPage()
 {
-    const [messages, setMessages] = useState<string[]>(["Hello","World"]);
+    const [messages, setMessages] = useState<MessageType[]>([{message: "Welcome to the chat", sender: 0}, {message: "Hello", sender: 1}, {message: "Hi", sender: 2, senderName: "John"}]);
 
     const [input, setInput] = useState("");
 
     async function sendMessage(e:React.FormEvent<HTMLFormElement>)
     {
         e.preventDefault();
-
+        if(input === "") return;
         //API CALL HERE
 
-        setMessages([...messages, input]);
+        pushMessage({message: input, sender: 2});
         setInput("");
+    }
+
+    async function pushMessage(message:MessageType) {
+        setMessages([...messages, message]);
     }
 
     return (
         <main className="flex max-w-2xl mx-auto flex-col h-screen p-4 gap-2">
-            <div className="card p-4 grow">
+            <div className="card p-4 grow flex flex-col gap-2 overflow-auto">
                 {
-                    messages.map((message, index) => (
-                        <div key={index} className="">
-                            {message}
-                        </div>
-                    ))
+                    messages.map((message, index) => {
+                        if(message.sender === 0)
+                        {
+                            return <div key={index} className="text-center">{message.message}</div>
+                        }
+                        if(message.sender === 1)
+                        {
+                            return <div key={index} className="bg-primary min-w-12 text-center w-fit rounded p-2 self-end">{message.message}</div>
+                        }
+                        if(message.sender === 2)
+                        {
+                            return <div key={index}>
+                                <div className="text-left text-sm text-gray-500">{message.senderName}</div>
+                                <div key={index} className="bg-background2 min-w-12 text-center w-fit rounded p-2">{message.message}</div>
+                            </div>
+                        }
+                        return <></>
+                    })
                 }
             </div>
             <div className="p-4 card flex flex-col gap-4">
--- client/src/pages/RoomPage.tsx
+++ client/src/pages/RoomPage.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import Button from "../components/common/Button"
 import Input from "../components/common/Input"
 
@@ -14,7 +15,10 @@ function RoomPage(props:{connectionStatus: boolean,socket: any}) {
         <main className="py-24">
             <form onSubmit={joinRoom} className="max-w-lg p-4 mx-auto card flex flex-col gap-4">
                 <Input label="Room Code" />
-                <Button disabled={!props.connectionStatus}>Enter Room</Button>
+                <Button disabled={!props.connectionStatus}>Join Room</Button>
+                <Link to="/chat" className="text-center hover:text-primary duration-100">
+                    Create a new room
+                </Link>
             </form>
         </main>
     )
