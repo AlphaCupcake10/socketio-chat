90877c2fbf1cfffbbc30e1ebd1f2b75988ae514a
AlphaCupcake10 <alphacupcake@outlook.com>
2024-03-21 01:09:38 +0530

    Update socket URL to use deployed server

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -1,6 +1,5 @@
 import { io } from 'socket.io-client';
 
-// "undefined" means the URL will be computed from the `window.location` object
-const URL = 'http://localhost:3000';
+const URL = 'https://socketio-chat-server-iota.vercel.app/';
 
 export const socket = io(URL);
